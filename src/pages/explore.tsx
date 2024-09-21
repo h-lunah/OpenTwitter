1355c179c1e9bdc09296229555835f05959a9e5a
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-09-21 08:28:13 +0000

    fix: new implementation

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,13 +1,16 @@
 import {
   query,
+  where,
   orderBy,
   startAt,
-  endAt
+  endAt,
+  limit
 } from 'firebase/firestore';
 import { useEffect, useState, type ReactElement, type ReactNode } from 'react';
 import { useCollection } from '@lib/hooks/useCollection';
 import { usersCollection } from '@lib/firebase/collections';
 import { useDebounce } from '@lib/hooks/useDebounce';
+import { useAuth } from '@lib/context/auth-context';
 import { MainContainer } from '@components/home/main-container';
 import {
   ExploreLayout,
@@ -17,28 +20,47 @@ import { MainLayout } from '@components/layout/main-layout';
 import { UserCard } from '@components/user/user-card';
 import { UserSearchBar } from '@components/user/user-search';
 import { MainHeader } from '@components/home/main-header';
-import { UpdateUsername } from '@components/home/update-username';
 import type { User } from '@lib/types/user';
 
-export default function Search(): JSX.Element {
-  const [input, setInput] = useState('');
-  const [dataTest, setDataTes] = useState<User[] | null>(null);
+const UsersList: React.FC<{ users: User[] }> = ({ users }) => {
+  if (users.length === 0) return <p className='text-center'>No user found</p>;
+  
 
+  return (
+    <div>
+      {users?.map((user) => (
+        <UserCard key={user?.id} {...user} />
+      ))}
+    </div>
+  );
+};
+
+export default function SearchPage(): JSX.Element {
+  const [input, setInput] = useState('');
+
+  const [dataUsers, setDataUsers] = useState<User[]>([]);
+
+  const { user } = useAuth();
   const debouncedInput = useDebounce(input, 500);
 
   const { data: usersData, loading } = useCollection(
     query(
       usersCollection,
+      where('username', '!=', user?.username),
       orderBy('username'),
       startAt(debouncedInput),
-      endAt(debouncedInput + '\uf8ff')
+      endAt(debouncedInput + '\uf8ff'),
+      limit(5)
     ),
     { allowNull: true }
   );
 
   useEffect(() => {
-    if (usersData) setDataTes(usersData);
-    if (usersData?.length === 0) setDataTes([]);
+    if (usersData) {
+      setDataUsers(usersData);
+      return;
+    }
+    setDataUsers([]);
   }, [usersData]);
 
   return (
@@ -47,26 +69,18 @@ export default function Search(): JSX.Element {
         useMobileSidebar
         title='Search'
         className='flex items-center justify-between'
-      >
-        <UpdateUsername />
-      </MainHeader>
+      ></MainHeader>
 
-      <div className='container mx-auto p-4'>
+      <div className='container mx-auto py-4'>
         <UserSearchBar
           value={input}
           onChange={(e): void => setInput(e.target.value)}
         />
-        <section className='mt-6'>
+        <section className='card-base mt-4 py-2'>
           {loading ? (
-            <p>Searching...</p>
-          ) : usersData?.length === 0 ? (
-            <p className='text-center'>No user found</p>
+            <p className='p-5 text-center'>Searching...</p>
           ) : (
-            <div>
-              {dataTest?.map((user) => (
-                <UserCard key={user?.id} {...user} />
-              ))}
-            </div>
+            <UsersList users={dataUsers} />
           )}
         </section>
       </div>
@@ -74,7 +88,7 @@ export default function Search(): JSX.Element {
   );
 }
 
-Search.getLayout = (page: ReactElement): ReactNode => (
+SearchPage.getLayout = (page: ReactElement): ReactNode => (
   <ProtectedLayout>
     <MainLayout>
       <ExploreLayout>{page}</ExploreLayout>
