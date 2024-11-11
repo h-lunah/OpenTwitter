2e0770f64978aa72cc777dabbdcc73d3c62bcc4f
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-11-11 20:05:31 +0100

    fix: only allow <img> with DOMPurify

--- src/lib/twemoji.ts
+++ src/lib/twemoji.ts
@@ -1,5 +1,10 @@
 import DOMPurify from 'dompurify';
 
+const purifyConfig = {
+	ALLOWED_TAGS: ['img'],
+	ALLOWED_ATTR: ['style', 'href']
+};
+
 export function twemojiParse(input: string): string {
   const emojiRegex =
     /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E6}-\u{1F1FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
@@ -15,11 +20,11 @@ export function twemojiParse(input: string): string {
 
       const imgUrl = `https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.1.0/assets/72x72/${codePoints}.png`;
 
-      result += `<img style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em; display: inline-block;" src="${imgUrl}" alt="${char}" style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em; display: inline-block;" />`;
+      result += `<img style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em; display: inline-block;" src="${imgUrl}" alt="${char}" />`;
     } else result += char;
   });
 
-  return DOMPurify.sanitize(result);
+  return DOMPurify.sanitize(result, purifyConfig);
 }
 
 export function twemojiParseWithLinks(
@@ -62,9 +67,9 @@ export function twemojiParseWithLinks(
 
       const imgUrl = `https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/svg/${codePoints}.svg`;
 
-      result += `<img src="${imgUrl}" style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em; display: inline-block;" alt="${char}" aria-hidden="true"/>`;
+      result += `<img style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -.1em; display: inline-block;" alt="${char}" src="${imgUrl}"/>`;
     } else result += char;
   });
 
-  return DOMPurify.sanitize(result);
+  return DOMPurify.sanitize(result, purifyConfig);
 }
